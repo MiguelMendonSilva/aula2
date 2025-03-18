0796398bd27d3abc1d0e53458122949d44ec299c
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-03-18 19:07:50 -0300

    Add files via upload

---
+++ exerc4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima1 = letra + 1;
+  char proxima2 = letra + 2;
+  char proxima3 = letra + 3;
+  char proxima4 = letra + 4;
+  char proxima5 = letra + 5;
+  char proxima6 = letra + 6;
+  char proxima7 = letra + 7;
+  char proxima8 = letra + 8;
+  char proxima9 = letra + 9;
+  char proxima10 = letra + 10;
+  
+  printf("Caracteres: %c %c %c %c %c %c %c %c %c %c\n", proxima1, proxima2, proxima3, proxima4,
+  proxima5, proxima6, proxima7, proxima8, proxima9, proxima10);
+  return 0;
+}  
